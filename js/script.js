5622f0a1eff6b90ad2036cada6ff1b5096fcb179
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-03-20 11:55:35 +0100

    Add scripts related to calculation of the triangle area

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var a = prompt('Enter a size of the triangle side'),
+	 h = prompt('Enter a height of the triangle'),
+	 triangleArea; 
+
+triangleArea = a * h / 2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
